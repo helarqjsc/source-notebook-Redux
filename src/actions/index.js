c4aae00466669e0440d876651f92b280b1903d73
helarqjsc <helarqjsc@gmail.com>
2015-08-18 21:25:56 +0500

    fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,50 @@
-export {
-    addNote,
-    deleteNote,
-    getNotes,
-    fetchNotes,
-    openNote,
-    closeNote,
-    saveNote,
-} from './notes.js';
+
+export function openNote(note) {
+  return {
+    type: 'OPEN_NOTE',
+    note,
+  };
+}
+
+export function closeNote() {
+  return {
+    type: 'CLOSE_NOTE',
+  };
+}
+
+export function getNotes(data) {
+  return {
+    type: 'GET_NOTES',
+    data,
+  };
+}
+
+export function saveNote(note) {
+  return {
+    type: 'SAVE_NOTE',
+    note,
+  };
+}
+
+export function addNote(note) {
+  return {
+    type: 'ADD_NOTE',
+    note,
+  };
+}
+
+export function fetchNotes() {
+  return dispatch => {
+    fetch('/db/data.json')
+      .then(res =>
+        res.json().then(data => dispatch(getNotes(data)))
+      );
+  }
+}
+
+export function deleteNote(note) {
+  return {
+    type: 'DELETE_NOTE',
+    note,
+  };
+}
