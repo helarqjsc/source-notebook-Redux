a7c6e26e77adcaad5629ca86b8d154529965d39a
helarqjsc <helarqjsc@gmail.com>
2015-08-27 18:36:19 +0500

    tab -> space

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,14 @@
 import Hotkeys from './hotkeys';
 import Tray from './tray';
 import { fetchConfig } from 'actions/config';
+import hljs from 'highlight.js/lib/highlight';
+
 
 export let configInit = (dispatch) => {
   dispatch(fetchConfig((data) => {
+    hljs.configure({
+      tabReplace: '  ',
+    });
     nw && Hotkeys.init(data);
     nw && Tray.init(data);
   }));
