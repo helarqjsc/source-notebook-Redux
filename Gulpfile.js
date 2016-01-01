bc2907b3344cea476656696ff024efdd8f9fbfda
helarqjsc <helarqjsc@gmail.com>
2016-01-01 22:16:01 +0500

    remove -d

--- Gulpfile.js
+++ Gulpfile.js
@@ -140,7 +140,7 @@ gulp.task('build-copy', function() {
 });
 
 gulp.task('build-js', shell.task([
-  'webpack -d --config webpack/nw.config.js'
+  'webpack -p --config webpack/nw.config.js'
 ]));
 
 gulp.task('build-clean', function () {
